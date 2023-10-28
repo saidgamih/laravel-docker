233a005ed100eef161afe08c265398848a918785
Said Gamih <said.gamih@gmail.com>
2023-10-28 15:35:31 +0100

    init

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>Tobedocker</title>
+
+    </head>
+    <body class="antialiased">
+        <h1>Hello world</h1>
+        <div>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ut, facere ratione. Dolorum, earum in asperiores architecto corporis animi. Officiis voluptas eos quo recusandae illum accusamus nobis impedit debitis magni saepe!</div>
+    </body>
+</html>
